88a0508185ddfddef261490de8d0d8136a8cee35
w68378 <w68378@student.wsiz.edu.pl>
2022-11-06 14:17:54 +0100

    [LAB4]

--- [LAB4] Zadanie 3.py
+++ [LAB4] Zadanie 3.py
@@ -1,25 +1,6 @@
-import random
-x = int(input("Podaj liczbę elementów: "))
-lista1 = []
+zwierzęta = []
+for x in range (3):
+    a = input("Podaj nazwę zwierząt: ")
+    zwierzęta.append(a)
 
-for i in range (x):
-    n = random.randint(1,10)
-    lista1.append(n)
-print(lista1)
-x1 = int(input("Podaj liczbe elementow dla drugiej listy: "))
-lista2 = []
-
-for i in range (x1):
-    n2 = random.randint(5,15)
-    lista2.append(n2)
-print(lista2)
-x3 = int(input("Podaj liczbe której szukasz: "))
-
-if x3 in lista1:
-    print("Liczba występuje w lista1")
-
-elif x3 in lista2:
-    print("Liczba występuje w lista2")
-
-else:
-    print("Nie ma takiej liczby w obu zestawach")
+print(zwierzęta)
